237e1b089846c8932c4d735598e00bb644a81158
Vladyslav <Vladyslav_Istomin@epam.com>
2015-11-04 08:43:13 +0200

    committ

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-var http = require('http-server');
-http.createServer(function(req,res) {
-	console.log("Hi")
-}).listen(3000)
+module.exports = function() {
+	var http = require('http-server');
+	http.createServer(function(req,res) {
+		console.log("Hi")
+	}).listen(3000)
+}
